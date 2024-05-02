6788e2793625fff845c771f061dd3a9bf3c9d6a0
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-02 17:03:11 +0530

    third

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 function App() {
+  const [place,setPlace] = useState();
+  const [date,setDate] = useState();
+  const [budget,setBudget] = useState();
+  const [content,setContent] = useState();
+  const [post,setPost] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000")
+    .then(posts => {setPost(posts.data)})
+    .catch(err => {console.log(err)})
+  },[]);
+  const submitForm=() => {
+    axios.post("http://localhost:5000",{place,date,budget,content})
+  }
   return (
     <>
-      <h1>Welcome to Client side</h1>
+          <center>
+            <form onSubmit={submitForm}>
+              <input type="text" placeholder="place" onChange={(e) => setPlace(e.target.value)}></input><br></br>
+              <input type="date" placeholder="date" onChange={(e) => setDate(e.target.value)}></input><br></br>
+              <input type="text" placeholder="budget" onChange={(e) => setBudget(e.target.value)}></input><br></br>
+              {/* <input type="textfield" placeholder="Write your experience" onChange={(e) => setContent(e.target.value)}></input><br></br> */}
+              <textarea rows={5} cols={50} placeholder="Write your experience" onChange={(e) => setContent(e.target.value)}></textarea>
+              <button type="submit">Post</button>
+            </form>
+          </center>
+      {
+        post.map((data) => {
+          return(
+          <>
+            <table>
+              <th>
+                <td color="red">{data.place}</td>
+                <td>{data.budget}</td>
+                <td>{data.date}</td>
+              </th>
+              <tr>
+                <td>{data.content}</td>
+              </tr>
+            </table>
+            
+          </>
+          )
+        })
+      }
     </>
   );
 }
