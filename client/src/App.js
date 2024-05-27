551b48f3741475887dc23a7331f027bd865cce61
RACHA NIKHIL <21955A1206@iare.ac.in>
2024-05-27 14:15:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,12 @@ function App() {
   const [content,setContent] = useState();
   const [post,setPost] = useState([]);
   useEffect(() => {
-    axios.get("https://localhost:5000/getInfo")
+    axios.get("https://backend-zeta-lovat.vercel.app/")
     .then(posts => {setPost(posts.data)})
     .catch(err => {console.log(err)})
   },[]);
   const submitForm=() => {
-    axios.post("https://localhost:5000/getInfo",{place,date,budget,content})
+    axios.post("https://backend-zeta-lovat.vercel.app/",{place,date,budget,content})
   }
   return (
     <>
