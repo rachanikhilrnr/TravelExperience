6d59ef913b23050cc35acd870780fba19e9e5aa5
RACHA NIKHIL <21955A1206@iare.ac.in>
2024-05-28 09:02:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = 5000;
 const app = express();
 app.use(cors(
     {
-        origin:["https://travel-experience-frontend.vercel.app/post"],
+        origin:["https://travel-experience-frontend.vercel.app/post","https://localhost:5000"],
         method:["POST","GET"],
         credentials:true
     }
